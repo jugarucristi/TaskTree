a604fed7082fdd2bfbfec763cf37428878ef6668
Cristi Jugaru <31081854+jugarucristi@users.noreply.github.com>
2023-05-30 18:01:56 +0300

    Initialized Frontend and Backend projects

---
+++ TaskTree.API/TaskTree.API/Controllers/TaskController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskTree.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+     
+    }
+}
